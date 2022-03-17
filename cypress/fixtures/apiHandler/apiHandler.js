8f93895e038aad510e4d3c0b5b4544d0d0862a00
Sheetal Bisht <sheetal.bisht@tajawal.com>
2022-03-17 16:18:16 +0400

    Update apiHandler.js

--- cypress/fixtures/apiHandler/apiHandler.js
+++ cypress/fixtures/apiHandler/apiHandler.js
@@ -2,9 +2,6 @@ export function interceptLoginApi() {
     cy.intercept('POST', 'api/account/v2/auth/local').as('loginAPI');
 }
 
-//export function interceptUpdateProfileApi(userId) {
-//    cy.intercept('POST', 'api/account/v2/user/'+userId).as('userProfileAPI');
-
 export function interceptUserProfileApi() {
-    cy.intercept('POST', 'api/account/v2/user/6226fe127c894a121a1c28c4').as('userProfileAPI');
+    cy.intercept('POST', 'api/account/v2/user/**').as('userProfileAPI');
 }
